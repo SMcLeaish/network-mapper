15ee74af073a8adb5aeb28f1879a25b2d04d14bf
CTWills <cameron.wills.1998@gmail.com>
2023-07-21 14:21:57 -0600

    created responsibilites table and role table

--- server/seeds/03_organization.js
+++ server/seeds/03_organization.js
@@ -4,8 +4,12 @@ exports.seed = function(knex) {
   return knex('organization').del()
     .then(function () {
       const organizations = Array.from({ length: 10 }, () => {
+        let latitude = faker.location.latitude()
+        let longitude = faker.location.longitude()
+        let location = [latitude, longitude]
         return {
-          name: faker.company.name()
+          name: faker.company.name(),
+          location: location
         };
       });
       return knex('organization').insert(organizations);
