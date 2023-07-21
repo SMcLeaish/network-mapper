5a23f84a601141d9d84f1911dede679c98aa4e70
CTWills <cameron.wills.1998@gmail.com>
2023-07-21 13:26:36 -0600

    migration table now creates individual table

--- server/migrations/20230721190359_create_individual_table.js
+++ server/migrations/20230721190359_create_individual_table.js
@@ -3,13 +3,13 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
-  return knex.schema.hasTable('individual').then((exists) => {
+    return knex.schema.hasTable('individual').then((exists) => {
     if (!exists) {
         return knex.schema.createTable('individual', table => {
             table.increments('id');
             table.string('name', 250);
             table.string('phone_number', 250);
-            table.specificType('location', 'number[]')
+            table.specificType('location', 'integer[]')
         })
     }
   })
