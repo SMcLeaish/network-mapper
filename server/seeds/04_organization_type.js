16adced5cb7c0d1a789b503f8304b9fa18b90c37
CTWills <cameron.wills.1998@gmail.com>
2023-07-25 08:59:46 -0600

    removed n/a field from org type

--- server/seeds/04_organization_type.js
+++ server/seeds/04_organization_type.js
@@ -2,7 +2,6 @@ exports.seed = function(knex) {
     return knex('organization_type').del()
       .then(function () {
         return knex('organization_type').insert([
-          {type: 'N/A'},
           {type: 'ngo'},
           {type: 'corporation'},
           {type: 'military'}
