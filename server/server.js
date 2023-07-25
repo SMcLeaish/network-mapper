520e5d6a01ca8ba37b071297c97c5a1486511bb3
CTWills <cameron.wills.1998@gmail.com>
2023-07-25 09:18:34 -0600

    added endpoints to get all users and orgs

--- server/server.js
+++ server/server.js
@@ -58,24 +58,24 @@ app.post('/users/login', (req, res) => {
     .catch(err => res.status(500).send(err))
 });
 
-app.get('/entity/:name', (req, res) => {
-  let { name } = req.params
-  knex.select('entity.id AS individual_entity_id', 'individual.name', 'individual.location AS individual_location', 'interaction.weight', 
-  'interaction.id_entity_1', 'interaction.id_entity_2', 'interaction.id_event', 'event.event_name', 
-  'event.location AS event_location', 'event.date AS event_date', 'event_type.type AS event_type', 'user_data.username', 'user_data.user_organization')
-    .from('individual')
-    .where({'individual.name': name})
-    .join('entity', 'individual.id', 'entity.id_individual')
-    .join('interaction', function() {
-      this 
-        .on('id_entity_1', '=', 'entity.id')
-        .orOn('id_entity_2', '=', 'entity.id')
-    })
-    .join('event', 'interaction.id_event', 'event.id')
-    .join('event_type', 'event_type_id', 'event_type.id')
-    .join('user_data', 'individual.id_user_data', 'user_data.id')
-    .then((data) => res.status(200).json(data))
-})
+// app.get('/entity/:name', (req, res) => {
+//   let { name } = req.params
+//   knex.select('entity.id AS individual_entity_id', 'individual.name', 'individual.location AS individual_location', 'interaction.weight', 
+//   'interaction.id_entity_1', 'interaction.id_entity_2', 'interaction.id_event', 'event.event_name', 
+//   'event.location AS event_location', 'event.date AS event_date', 'event_type.type AS event_type', 'user_data.username', 'user_data.user_organization')
+//     .from('individual')
+//     .where({'individual.name': name})
+//     .join('entity', 'individual.id', 'entity.id_individual')
+//     .join('interaction', function() {
+//       this 
+//         .on('id_entity_1', '=', 'entity.id')
+//         .orOn('id_entity_2', '=', 'entity.id')
+//     })
+//     .join('event', 'interaction.id_event', 'event.id')
+//     .join('event_type', 'event_type_id', 'event_type.id')
+//     .join('user_data', 'individual.id_user_data', 'user_data.id')
+//     .then((data) => res.status(200).json(data))
+// })
 
 app.get('/narratives/:id', (req, res) => {
   let { id } = req.params;
@@ -84,6 +84,18 @@ app.get('/narratives/:id', (req, res) => {
     .where(function() {
       this.where({'id_entity': id}).orWhere({'id_event': id})
     })
+    .then((data) => res.status(200).json(data))
+})
+
+app.get('/individuals', (req, res) => {
+  knex.select('*')
+    .from('individual')
+    .then((data) => res.status(200).json(data))
+})
+
+app.get('/organizations', (req, res) => {
+  knex.select('*')
+    .from('organization')
     .then((data) => res.status(200).json(data))
 })
 
@@ -108,6 +120,32 @@ app.get('/position/:id', (req, res) => {
           .join('role', 'position.role_id', 'role.id')
           .join('responsibilities', 'role.responsibilities_id', 'responsibilities.id')
           .then(otherData => res.status(200).json(otherData))
+      }
+    })
+})
+
+app.get('/entity/:name', (req, res) => {
+  let { name } = req.params
+  knex.select('*')
+    .from('individual')
+    .where({'individual.name': name})
+    .then(data => {
+      if (data.length !== 0) {
+        knex.select('entity.id AS individual_entity_id', 'individual.name', 'individual.location AS individual_location', 'interaction.weight', 
+        'interaction.id_entity_1', 'interaction.id_entity_2', 'interaction.id_event', 'event.event_name', 
+        'event.location AS event_location', 'event.date AS event_date', 'event_type.type AS event_type', 'user_data.username', 'user_data.user_organization')
+          .from('individual')
+          .where({'individual.name': name})
+          .join('entity', 'individual.id', 'entity.id_individual')
+          .join('interaction', function() {
+            this 
+              .on('id_entity_1', '=', 'entity.id')
+              .orOn('id_entity_2', '=', 'entity.id')
+          })
+          .join('event', 'interaction.id_event', 'event.id')
+          .join('event_type', 'event_type_id', 'event_type.id')
+          .join('user_data', 'individual.id_user_data', 'user_data.id')
+          .then((data) => res.status(200).json(data))
       }
     })
 })
